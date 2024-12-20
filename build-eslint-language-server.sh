697cd08c0e77ebb8a648e9a40e1dff9ae57b4abe
Christopher Aubut <christopher@aubut.me>
2024-12-20 09:39:30 -0700

    chore: updates

--- build-eslint-language-server.sh
+++ build-eslint-language-server.sh
@@ -3,16 +3,16 @@
 # Check if --debug option is provided
 DEBUG_MODE=false
 if [ "$1" == "--debug" ]; then
-  DEBUG_MODE=true
+	DEBUG_MODE=true
 fi
 
 # Clone the repository
 rm -rf vscode-eslint
-git clone https://github.com/microsoft/vscode-eslint.git 
+git clone https://github.com/microsoft/vscode-eslint.git
 
 # Checkout the given release version
 cd vscode-eslint
-git checkout release/3.0.10
+git checkout main
 npm install
 
 # Build the eslint language server
@@ -22,10 +22,10 @@ npm run webpack
 
 # If not in debug mode, clean up unnecessary files
 if [ "$DEBUG_MODE" == "false" ]; then
-  cd ../../
-  echo "Cleaning up the repository except for ./vscode-eslint/server/out..."
-  find ./vscode-eslint -mindepth 1 ! -regex '^./vscode-eslint/server\(/.*\)?' -delete
-  find ./vscode-eslint/server -mindepth 1 ! -regex '^./vscode-eslint/server/out\(/.*\)?' -delete
+	cd ../../
+	echo "Cleaning up the repository except for ./vscode-eslint/server/out..."
+	find ./vscode-eslint -mindepth 1 ! -regex '^./vscode-eslint/server\(/.*\)?' -delete
+	find ./vscode-eslint/server -mindepth 1 ! -regex '^./vscode-eslint/server/out\(/.*\)?' -delete
 else
-  echo "Skipping cleanup due to --debug mode."
+	echo "Skipping cleanup due to --debug mode."
 fi
